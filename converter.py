28ecb5a990935b76760823ac10798e0cd6fa5696
scott5Tots <85150375+scott5Tots@users.noreply.github.com>
2023-11-28 03:18:09 -0500

    Update converter.py

--- converter.py
+++ converter.py
@@ -1,22 +1,21 @@
 from flask import Flask, request, jsonify
-from forex_python.converter import CurrencyRates, CurrencyCodes
+from forex_python.converter import CurrencyRates
 
 app = Flask(__name__)
 
 @app.route('/convert', methods=['POST'])
 def convert_currency():
     data = request.json
+    currency_rates = CurrencyRates()
     amount = data['amount']
     from_currency = data['from_currency']
     to_currency = data['to_currency']
-
-    c = CurrencyRates()
     try:
-        converted_amount = c.convert(from_currency, to_currency, amount)
-        return jsonify({"converted_amount": converted_amount})
+        converted_amount = currency_rates.convert(from_currency, to_currency, amount)
+        response = {"converted_amount": converted_amount}
     except Exception as e:
-        return jsonify({"error": str(e)})
-
+        response = {"error": str(e)}
+    return jsonify(response)
 
 if __name__ == "__main__":
-    app.run(port=5000)
+    app.run(host='0.0.0.0', port=5000)
